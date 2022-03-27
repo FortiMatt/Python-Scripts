15ea73a77a60c551cb82d574a42a1904da638d86
FortiMatt <52750829+FortiMatt@users.noreply.github.com>
2022-03-26 21:05:36 -0400

    Create base_file.py
    Start of Parser File

---
+++ FortiOS_Parser/base_file.py
@@ -0,0 +1,38 @@
+import csv
+
+csv.register_dialect('skip_space', skipinitialspace=True)
+
+
+def indenter(x):
+    indent = "\t"*x
+    return indent
+
+
+with open('9570_UNION_PARK_7-0_0234_202203221506.txt', 'r') as f:
+    reader = csv.reader(f, delimiter=' ', dialect='skip_space')
+    conf_file_name = []
+    tab = 0
+    for item in reader:
+        # COPY AND APPENDS CONF FILE SERIAL/VERSION/BUILD NUMBER
+        if item[0].startswith("#"):
+            conf_file_name.append(item)
+        elif item[0] == "config" and tab == 0:
+            print(item)
+            tab += 1
+        elif item[0] == "config" and tab > 0:
+            tab += 1
+            print(indenter(tab), item)
+        elif item[0] == "end" and tab == 1:
+            print(item)
+            tab = 0
+        elif item[0] == "end" and tab > 1:
+            tab -= 1
+            print(indenter(tab), item)
+        elif item[0] == "edit" and tab == 1:
+            print(indenter(tab), item)
+            tab += 1
+        elif item[0] == "next" and tab == 2:
+            tab -= 1
+            print(indenter(tab), item)
+        else:
+            print(indenter(tab), item)
